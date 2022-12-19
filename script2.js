878b7c1c62501344d87d640c93890bab39d4eca8
scholtzk <scholtzk@hotmail.com>
2022-12-20 09:06:58 +1300

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,55 +1,49 @@
-// Get a reference to the canvas element
-const canvas = document.getElementById("myCanvas");
+// ask chatgpt to finish this code
+const images = document.querySelectorAll(".image-container img");
 
-// Get the context of the canvas element
-const ctx = canvas.getContext("2d");
-
-// Create a grid of points that we will use to warp the image
-const gridSize = 10;
-const grid = [];
-for (let i = 0; i < gridSize; i++) {
-  for (let j = 0; j < gridSize; j++) {
-    grid.push({
-      x: j * (canvas.width / gridSize),
-      y: i * (canvas.height / gridSize),
-      originalX: j * (canvas.width / gridSize),
-      originalY: i * (canvas.height / gridSize),
-    });
-  }
-}
-
-// Load the image that we will warp
-const image = new Image();
-image.src = "path/to/image.jpg";
-image.onload = function () {
-  // Draw the image onto the canvas
-  ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-};
-
-// Warp the image based on the mouse position
 document.addEventListener("mousemove", (event) => {
-  // Calculate the mouse position relative to the canvas
-  const mouseX = event.clientX - canvas.offsetLeft;
-  const mouseY = event.clientY - canvas.offsetTop;
+  // Get the mouse position
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
 
-  // Update the position of each point in the grid
-  grid.forEach((point) => {
-    // Calculate the distance between the mouse and the point
-    const dx = mouseX - point.x;
-    const dy = mouseY - point.y;
+  images.forEach((image) => {
+    // Get the bounding rect of the image
+    const rect = image.getBoundingClientRect();
+    // Calculate the center of the image
+    const imageX = rect.left + rect.width / 2;
+    const imageY = rect.top + rect.height / 2;
+    // Calculate the distance between the mouse and the center of the image
+    const dx = mouseX - imageX;
+    const dy = mouseY - imageY;
     const distance = Math.sqrt(dx * dx + dy * dy);
 
-    // If the mouse is within 100 pixels of the point, move the point away from the mouse
-    if (distance < 100) {
-      point.x = point.originalX + dx / distance * 100;
-      point.y = point.originalY + dy / distance * 100;
-    } else {
-      // Otherwise, move the point back to its original position
-      point.x = point.originalX;
-      point.y = point.originalY;
-    }
-  });
+    // Define the number of rows and columns in the grid
+    const rows = 10;
+    const columns = 10;
+    // Calculate the size of each cell in the grid
+    const cellWidth = rect.width / columns;
+    const cellHeight = rect.height / rows;
 
-  // Redraw the image using the warped grid
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(image, 0, 0, canvas.width,
+    // Create a 2D array to store the positions of each cell in the grid
+    const grid = [];
+    for (let i = 0; i < rows; i++) {
+      grid[i] = [];
+      for (let j = 0; j < columns; j++) {
+        grid[i][j] = {
+          x: j * cellWidth,
+          y: i * cellHeight,
+        };
+      }
+    }
+
+    // Warp the image by modifying the positions of each cell in the grid
+    if (distance < 100) {
+      // If the mouse is within 100 pixels of the image, warp the image
+      for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < columns; j++) {
+          // Calculate the distance between the cell and the mouse
+          const cellX = grid[i][j].x + rect.left + cellWidth / 2;
+          const cellY = grid[i][j].y + rect.top + cellHeight / 2;
+          const dx = mouseX - cellX;
+          const dy = mouseY - cellY;
+          const cellDistance = Math.sqrt(dx * dx + dy * dy);
